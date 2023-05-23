5b04eea07b35e499451dc2ecaf3b21674c2744e6
swell-d <3952800@gmail.com>
2023-05-23 23:24:00 +0200

    logic

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -8,37 +8,37 @@ allianz = {
     'brand': 'Allianz',
     'logo': 'allianz.svg',
     'description': 'Allianz description',
-    'price': '123€ в месяц'
+    'price': 123
 }
 barmenia = {
     'brand': 'Barmenia',
     'logo': 'barmenia.svg',
     'description': 'Barmenia description',
-    'price': '234€ в месяц'
+    'price': 234
 }
 diebayerische = {
     'brand': 'die Bayerische',
     'logo': 'diebayerische.svg',
     'description': 'die Bayerische description',
-    'price': '345€ в месяц'
+    'price': 345
 }
 muenchenerverein = {
     'brand': 'Münchener Verein',
     'logo': 'muenchener-verein.svg',
     'description': 'Münchener Verein description',
-    'price': '456€ в месяц'
-}
-signaliduna = {
-    'brand': 'SIGNAL IDUNA',
-    'logo': 'signal-iduna.svg',
-    'description': 'SIGNAL IDUNA description',
-    'price': '567€ в месяц'
+    'price': 456
 }
 nuernberger = {
     'brand': 'NÜRNBERGER',
     'logo': 'nuernberger.svg',
     'description': 'NÜRNBERGER description',
-    'price': '678€ в месяц'
+    'price': 567
+}
+signaliduna = {
+    'brand': 'SIGNAL IDUNA',
+    'logo': 'signal-iduna.svg',
+    'description': 'SIGNAL IDUNA description',
+    'price': 678
 }
 
 
@@ -60,11 +60,35 @@ def main_routes(app):
 
     @app.route('/calculate', methods=['GET'])
     def calculate():
-        age = request.args.get('age') if request.args.get('age') else 18
-        teeth = request.args.get('teeth') if request.args.get('teeth') else 0
+        age = int(request.args.get('age')) if request.args.get('age') else 18
+        teeth = int(request.args.get('teeth')) if request.args.get('teeth') else 0
         prosthodontics = request.args.get('prosthodontics', '')
         orto = request.args.get('orto', '')
 
-        data = {'cards': [allianz, barmenia, diebayerische, muenchenerverein, signaliduna, nuernberger]}
+        data = {'cards': []}
+        if age >= 18 and teeth == 0 and orto == '':
+            data = {'cards': [allianz, barmenia, diebayerische, muenchenerverein, nuernberger, signaliduna]}
+        elif age >= 18 and teeth == 1 and orto == '':
+            allianz25 = allianz.copy()
+            allianz25['price'] *= 1.25
+            data = {'cards': [barmenia, diebayerische, allianz25, muenchenerverein, nuernberger, signaliduna]}
+        elif age >= 18 and teeth == 2 and orto == '':
+            allianz50 = allianz.copy()
+            allianz50['price'] *= 1.5
+            data = {'cards': [barmenia, allianz50, signaliduna, diebayerische, muenchenerverein, nuernberger]}
+        elif age >= 18 and teeth == 3 and orto == '':
+            allianz75 = allianz.copy()
+            allianz75['price'] *= 1.75
+            data = {'cards': [allianz75, barmenia, signaliduna, diebayerische, muenchenerverein, nuernberger]}
+        elif age >= 18 and teeth >= 4 and orto == '':
+            data = {'cards': [nuernberger]}
+
+        elif age >= 18 and orto != '':
+            data = {'cards': [diebayerische, muenchenerverein]}
+
+        elif age < 18 and orto == '':
+            data = {'cards': [allianz, barmenia, diebayerische, muenchenerverein, nuernberger, signaliduna]}
+        elif age < 18 and orto != '':
+            data = {'cards': [allianz, signaliduna, barmenia, diebayerische, muenchenerverein, nuernberger]}
 
         return render_template('results.html', **data)
