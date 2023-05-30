ed3cd21638a884d8aca364da38e20f976e5eaf72
swell-d <3952800@gmail.com>
2023-05-30 22:36:49 +0200

    security

--- server.py
+++ server.py
@@ -1,14 +1,26 @@
 import os
 
 from flask import Flask
+from flask_talisman import Talisman
 
 from instance import companies, allianz_unlimited, allianz, barmenia2, barmenia3, diebayerische, muenchenerverein, \
     signaliduna
 from routes import error, main_routes
 
+csp = {
+    'default-src': "'self'",
+    'img-src': ["'self'", 'data:'],
+    'style-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net', '*.fontawesome.com'],
+    'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net', '*.fontawesome.com'],
+    'connect-src': ["'self'", '*.fontawesome.com'],
+    'font-src': ["'self'", '*.fontawesome.com']
+}
+talisman = Talisman()
+
 
 def create_app():
     app = Flask(__name__)
+    talisman.init_app(app, content_security_policy=csp)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or 'my-secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 600
@@ -34,21 +46,8 @@ def create_app():
         muenchenerverein.create(db)
         signaliduna.create(db)
 
-    @app.after_request
-    def add_header(response):
-        response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
-        response.headers['X-Frame-Options'] = 'SAMEORIGIN'
-        response.headers['Content-Security-Policy'] = \
-            "default-src 'self'; " \
-            "img-src 'self' data: ; " \
-            "style-src 'self' 'unsafe-inline' cdn.jsdelivr.net *.fontawesome.com; " \
-            "script-src 'self' 'unsafe-inline' cdn.jsdelivr.net *.fontawesome.com; " \
-            "connect-src 'self' *.fontawesome.com; " \
-            "font-src 'self' *.fontawesome.com;"
-        return response
-
     return app
 
 
 if __name__ == '__main__':
-    create_app().run(debug=False)
+    create_app().run(debug=True)
